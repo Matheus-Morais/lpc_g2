cdff3523d93b94a5a6f8dd7c26d65cae3cc01138
Fabio Castro <deliberado@gmail.com>
2017-06-19 18:07:15 -0300

    first commit

---
+++ evento/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(EventoCientifico)
+admin.site.register(Pessoa)
+admin.site.register(Autor)
+admin.site.register(ArtigoAutor)
+admin.site.register(ArtigoCientifico)
+admin.site.register(Inscricoes)
+admin.site.register(Avaliador)
+admin.site.register(AvaliacaoArtigo)
